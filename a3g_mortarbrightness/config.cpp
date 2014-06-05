dcb5b392ce6b0f778fe0dcfb6a7323d986cd2b45
Cephei <cepheidev+github@gmail.com>
2014-06-05 03:24:28 +0200

    Initial commit
    Added fixed mortarbrightness
    Added flavor icons ( faction themed ) nvg icons for opfor and indfor

---
+++ a3g_mortarbrightness/config.cpp
@@ -0,0 +1,23 @@
+class CfgPatches {
+	class a3g_mortarbrightness {
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"A3_Data_F",
+			"A3_Weapons_F",
+			"A3_Weapons_F_EPA_Acc"
+		};
+		author[]=
+		{
+			"akp", "Cephei"
+		};
+	};
+};
+class CfgAmmo {
+	class FlareCore;
+	class Flare_82mm_AMOS_White : FlareCore {
+		brightness=16;
+	};
+};
---
+++ a3g_nvgflavoricons/config.cpp
@@ -0,0 +1,26 @@
+class CfgPatches {
+	class a3g_nvgflavoricons {
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"A3_Data_F",
+			"A3_Weapons_F",
+			"A3_Weapons_F_EPA_Acc"
+		};
+		author[]=
+		{
+			"akp", "Cephei"
+		};
+	};
+};
+class CfgWeapons {
+	class NVGoggles;
+	class NVGoggles_OPFOR: NVGoggles {
+		picture="\a3g_nvgflavoricons\gear_nvg_opf_CA.paa";
+	};
+	class NVGoggles_INDEP: NVGoggles {
+		picture="\a3g_nvgflavoricons\gear_nvg_indp_CA.paa";
+	};
+};
